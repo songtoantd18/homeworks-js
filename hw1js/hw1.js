7911dfd9a85f7f5727fe5ffcc7848108e4953fcd
songtoantd18 <songtoantd18@gmail.com>
2022-05-18 15:12:45 +0700

    ex2

---
+++ hw1js/hw1.js
@@ -0,0 +1,9 @@
+document.getElementById('change-content').onclick=()=>{
+    document.getElementById('hello').innerText="goodbye to the worlds"
+}
+function example1() {
+    prompt("xin chao");
+  }
+  function example2() {
+    alert("nhap ten cua ban");
+  }
